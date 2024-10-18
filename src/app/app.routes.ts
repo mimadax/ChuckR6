b619820f4e85fec70409c84ac32f85c9e0ead08d
mimadax <andreasoares38@gmail.com>
2024-10-18 12:29:55 +0200

    navbar ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PagePanelComponent } from './page-panel/page-panel.component';
+import { PageContactComponent } from './page-contact/page-contact.component';
+import { PageHomeComponent } from './page-home/page-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: PageHomeComponent}, 
+    {path: 'panel', component: PagePanelComponent},
+    {path: 'contact', component: PageContactComponent},
+];
---
+++ src/app/page-panel/page-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-panel.component.html',
+  styleUrl: './page-panel.component.css'
+})
+export class PagePanelComponent {
+
+}
