018070effd7ee9de71719273b2a91668a66c9f49
mimadax <andreasoares38@gmail.com>
2024-10-18 12:06:13 +0200

    installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,27 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ChuckR6';
+export class AppComponent implements OnInit {
+
+/**
+ * 
+ * VARIABLE GLOABLE 
+ * 
+ */
+
+APP_ENV: string ="DEV"; //on instencie une variable pour dit si on est en PROD or DEV
+API_URL: string = ""; //on laisse vite mais on va implamenter OnInti pour qu'il remplisse cette variable a chaque fois
+
+ngOnInit(): void {
+    /** GESTION API */
+    if (this.APP_ENV == "DEV") {
+      this.API_URL = "http://localhost:8080/api";
+    }else if(this.APP_ENV == "PROD") {
+      this.API_URL = "http://localhost:8080/api";
+    }else{
+      this.API_URL = "http://localhost:8080/api";
+    }
 }
+
+}
+
